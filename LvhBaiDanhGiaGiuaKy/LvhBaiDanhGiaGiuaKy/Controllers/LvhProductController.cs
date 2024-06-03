f8ede1ea5d625dc1abc4ab0b2f2d0023ffdb580c
unknown <levinhhuy113@gmail.com>
2024-06-03 11:08:52 +0700

    add file

---
+++ LvhBaiDanhGiaGiuaKy/LvhBaiDanhGiaGiuaKy/Controllers/LvhProductController.cs
@@ -0,0 +1,106 @@
+﻿using LvhBaiDanhGiaGiuaKy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LvhBaiDanhGiaGiuaKy.Controllers
+{
+    public class LvhProductController : Controller
+    {
+        private static List<LvhProduct> lvhProducts = new List<LvhProduct>()
+        {
+            new LvhProduct(){LvhId=106,LvhFullName="Lê Vinh Huy",LvhImage="1234",LvhQuantity=10,LvhPrice=1000000,LvhIsActive=true},
+            new LvhProduct(){LvhId=1,LvhFullName="Đỗ Trọng Thạo",LvhImage="1235",LvhQuantity=11,LvhPrice=2000000,LvhIsActive=true},
+
+        };
+        // GET: LvhProduct
+        public ActionResult LvhIndex()
+        {
+            return View(lvhProducts);
+        }
+        public ActionResult LvhCreate()
+        {
+            var lvhModel =new LvhProduct();
+            return View(lvhModel);
+        }
+        [HttpPost]
+        public ActionResult LvhCreate(LvhProduct lvhProduct)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(lvhProduct);
+            }
+            lvhProducts.Add(lvhProduct);
+            return RedirectToAction("LvhIndex");
+        }
+        public ActionResult LvhEdit(int id)
+        {
+            var product = lvhProducts.Find(p => p.LvhId == id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+        public ActionResult LvhDetails(int id)
+        {
+            var product = lvhProducts.Find(p => p.LvhId == id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LvhEdit(LvhProduct lvhProduct)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(lvhProduct);
+            }
+
+            var product = lvhProducts.Find(p => p.LvhId == lvhProduct.LvhId);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+
+            // Cập nhật các giá trị
+            product.LvhFullName = lvhProduct.LvhFullName;
+            product.LvhImage = lvhProduct.LvhImage;
+            product.LvhQuantity = lvhProduct.LvhQuantity;
+            product.LvhPrice = lvhProduct.LvhPrice;
+            product.LvhIsActive = lvhProduct.LvhIsActive;
+
+            return RedirectToAction("LvhIndex");
+        }
+        public ActionResult LvhDelete(int id)
+        {
+            var product = lvhProducts.Find(p => p.LvhId == id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        [HttpPost, ActionName("LvhDelete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult LvhDeleteConfirmed(int id)
+        {
+            var product = lvhProducts.Find(p => p.LvhId == id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            lvhProducts.Remove(product);
+            return RedirectToAction("LvhIndex");
+        }
+
+
+    }
+}
---
+++ LvhBaiDanhGiaGiuaKy/LvhBaiDanhGiaGiuaKy/Models/LvhProduct.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LvhBaiDanhGiaGiuaKy.Models
+{
+    public class LvhProduct
+    {
+        [Key]
+        public int LvhId { get; set; }
+        [DisplayName("LVH:Họ và Tên")]
+        [Required(ErrorMessage = "LVH: Chưa nhập dữ liệu")]
+        [StringLength(100, MinimumLength = 5, ErrorMessage = "LVH:Họ tên chứa tối thiểu 5 kí tự hoặc tối đa 100")]
+        [RegularExpression(@"^[A-Za-z ]{5,100}$", ErrorMessage = "Họ tên chỉ chứa ký tự viết hoa và khoảng trắng")]
+        public string LvhFullName { get; set; }
+        [DisplayName("LVH:Ảnh")]
+        [Required(ErrorMessage = "LVH: Chưa nhập dữ liệu")]
+        [DataType(DataType.ImageUrl, ErrorMessage = "Đth:Đây không phải là URL hình ảnh hợp lệ")]
+        public string LvhImage { get; set; }
+        [DisplayName("LVH:Số lượng")]
+        [Required(ErrorMessage = "LVH: Chưa nhập dữ liệu")]
+        [Range(1, 100, ErrorMessage = "LVH:Số lượng phải là số trong khoảng 1-100.")]
+        public int LvhQuantity { get; set; }
+        [DisplayName("LVH:Giá")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "LVH:Giá phải là số lớn hơn 0.")]
+        [Required(ErrorMessage = "LVH: Chưa nhập dữ liệu")]
+
+        public decimal LvhPrice { get; set; }
+        [DisplayName("LVH:Trạng thái")]
+        
+        
+        public bool LvhIsActive { get; set; } = true;
+    }
+}
